a59e54a29ca6b39c0366d6fcae2209b97f272dba
Petter Iversen <petter.iversen90@gmail.com>
2020-10-01 21:25:56 +0200

    Updated level-1.js
    Added level 2, q1

--- level-1.js
+++ level-1.js
@@ -128,3 +128,13 @@ function newSpecies(specimen) {
   fishInTheSea.push(specimen);
 }
 newSpecies("tuna");
+
+// level 2
+
+//  question 1: Using the loop from question 5 above, only log the counter variable if it is an even number.
+
+for (var count = 15; count <= 25; count++) {
+  if (count % 2 === 0) {
+    console.log(count);
+  }
+}
